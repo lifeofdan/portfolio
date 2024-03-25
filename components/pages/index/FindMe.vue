8c4c95e34ea25423cd63dbef4b9aadb37b8d0922
Daniel Newman <contact@lifeofdan.me>
2024-03-26 08:29:33 +1000

    Add links to socials

--- components/pages/index/FindMe.vue
+++ components/pages/index/FindMe.vue
@@ -2,24 +2,30 @@
   <div>
     <h2 class="uppercase mb-6">Find me on</h2>
     <div class="grid gap-4">
-      <div
-        class="flex justify-between border-b border-dotted hover:border-b-secondary"
-      >
-        <p class="text-md">Twitter</p>
-        <Icon icon="bx:bxl-twitter" class="text-2xl mb-1" />
-      </div>
-      <div
-        class="flex justify-between border-b border-dotted hover:border-b-secondary"
-      >
-        <p class="text-md inline-block align-middle">GitHub</p>
-        <Icon icon="bx:bxl-github" class="text-2xl mb-1" />
-      </div>
-      <div
-        class="flex justify-between border-b border-dotted hover:border-b-secondary"
-      >
-        <p class="text-md inline-block align-middle">Linkedin</p>
-        <Icon icon="bx:bxl-linkedin-square" class="text-2xl mb-1" />
-      </div>
+      <NuxtLink to="https://twitter.com/lifeofdandotme">
+        <div
+          class="flex justify-between border-b border-dotted hover:border-b-secondary"
+        >
+          <p class="text-md">Twitter</p>
+          <Icon icon="bx:bxl-twitter" class="text-2xl mb-1" />
+        </div>
+      </NuxtLink>
+      <NuxtLink to="https://github.com/lifeofdan">
+        <div
+          class="flex justify-between border-b border-dotted hover:border-b-secondary"
+        >
+          <p class="text-md inline-block align-middle">GitHub</p>
+          <Icon icon="bx:bxl-github" class="text-2xl mb-1" />
+        </div>
+      </NuxtLink>
+      <NuxtLink to="https://www.linkedin.com/in/lifeofdan">
+        <div
+          class="flex justify-between border-b border-dotted hover:border-b-secondary"
+        >
+          <p class="text-md inline-block align-middle">Linkedin</p>
+          <Icon icon="bx:bxl-linkedin-square" class="text-2xl mb-1" />
+        </div>
+      </NuxtLink>
     </div>
   </div>
 </template>
