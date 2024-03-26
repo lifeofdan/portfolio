0e9e1cfa6f09af9857ae003522f7bd8ed1c9c919
Daniel Newman <contact@lifeofdan.me>
2024-03-26 22:52:24 +1000

    Create markdownimg component to optimize images in articles

---
+++ components/global/MarkdownImg.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <NuxtPicture
+      :quality="quality"
+      format="webp"
+      :src="src"
+      :width="width"
+      :height="height"
+      :alt="alt"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps(["src", "width", "height", "alt", "quality"]);
+</script>
+
+<style scoped></style>
