c84ce5a4c31603a006e790d20ceef57f095ac745
Daniel Newman <contact@lifeofdan.me>
2025-03-20 00:17:01 -0400

    Update packages and update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   runtimeConfig: {
-    githubAccessToken: "",
+    githubAccessToken: ""
   },
+
   app: {
     head: {
       meta: [
-        { name: "description", content: "Portfolio site for Daniel Newman" },
+        { name: "description", content: "Portfolio site for Daniel Newman" }
       ],
       htmlAttrs: {
-        lang: "en",
-      },
-    },
+        lang: "en"
+      }
+    }
   },
+
   image: {
     quality: 80,
-    format: ["webp"],
+    format: ["webp"]
   },
+
   devtools: { enabled: true },
+
   modules: [
     "@nuxtjs/tailwindcss",
     "nuxt-icon-tw",
     "@nuxt/content",
-    "@nuxt/image",
+    "@nuxt/image"
   ],
+
+  compatibilityDate: "2025-03-20"
 });
